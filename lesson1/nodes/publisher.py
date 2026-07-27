f5af16459e27d3bcc3d99064731b82465fdc4686
DenzelTan <denzeltan02@gmail.com>
2026-07-27 13:47:15 +0300

    lesson 1 done

---
+++ lesson1/nodes/publisher.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+class Publisher:
+    def __init__(self):
+        # Internal variables
+        # Parameters
+        self.message = rospy.get_param('~message', 'Hello World!')
+        self.rate = rospy.get_param('~rate', 2.0)
+        self.rate = rospy.Rate(self.rate)
+        # Publishers
+        self.pub = rospy.Publisher('/message', String, queue_size=10)
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.pub.publish(self.message)
+            self.rate.sleep()
+
+if __name__ == '__main__':
+    rospy.init_node('publisher')
+    node = Publisher()
+    node.run()
